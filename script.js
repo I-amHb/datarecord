bc7da9f75def3c628f1d662254086b0cb3c35410
Habib <yusufhabib3@gmail.com>
2025-03-12 17:10:00 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const table = document.querySelector('.table');
+const nameInput = document.querySelector('.nameInput');
+const ageInput = document.querySelector('.ageInput');
+const btn = document.querySelector('.btn');
+
+
+const registryArr = [];
+  
+registryArr.map((val) => {
+  
+  let tabRow = document.createElement('tr');
+  tabRow.setAttribute('class', 'tableRow');
+  
+  let tabNameData = document.createElement('td');
+  tabNameData.setAttribute('class', 'nameData');
+  tabNameData.textContent = val.name;
+  
+  let tabAgeData = document.createElement('td');
+  tabAgeData.setAttribute('class', 'ageData');
+  tabAgeData.textContent = val.age;
+  
+  tabRow.appendChild(tabNameData);
+  tabRow.appendChild(tabAgeData);
+  
+  table.appendChild(tabRow);
+  console.log(tabRow);
+})
+
+
+const renderArr = () => {
+  
+  let latestEntry = registryArr[registryArr.length -1];
+  
+  let tabRow = document.createElement('tr');
+  tabRow.setAttribute('class', 'tableRow');
+  
+  let tabNameData = document.createElement('td');
+   tabNameData.setAttribute('class', 'nameData');
+  tabNameData.textContent = latestEntry.name;
+  
+  let tabAgeData = document.createElement('td');
+  tabAgeData.setAttribute('class', 'ageData');
+  tabAgeData.textContent = latestEntry.age;
+  
+  tabRow.appendChild(tabNameData);
+  tabRow.appendChild(tabAgeData);
+  
+  table.appendChild(tabRow);
+  
+};
+
+btn.addEventListener('click',  () => {
+   registryArr.push({name: nameInput.value, age: ageInput.value});
+  
+  renderArr();
+  
+  nameInput.value ='';
+  ageInput.value = '';
+  
+  
+  console.log(registryArr);
+  
+});
+
+
